93e4c0bdeefc90ff346ba6cadfeb370cc4b0c6d4
zxf <zxf@yc.com>
2024-10-24 14:25:19 +0800

    feat: 添加 Info.plist 配置文件， 设置软件开发地区 CFBundleDevelopmentRegion: Chinese (中文区域开发) , 解决 open 打开系统文件弹窗中的文字 英文 -> 中午

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,27 +1,10 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use tauri::{CustomMenuItem, Manager, Menu, MenuItem, Submenu};
-
+use tauri::{Manager};
 use tauri_plugin_log::LogTarget;
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 fn main() {
     let ctx = tauri::generate_context!();
 
-    let quit = CustomMenuItem::new("quit".to_string(), "退出");
-    let close = CustomMenuItem::new("close".to_string(), "关闭");
-    let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
-    let _menu = Menu::new()
-        .add_native_item(MenuItem::Copy)
-        .add_item(CustomMenuItem::new("hide", "Hide"))
-        .add_submenu(submenu);
-
-    // let menu = Menu::new(); // configure the menu
     tauri::Builder::default()
         .plugin(
             tauri_plugin_log::Builder::default()
@@ -37,8 +20,7 @@ fn main() {
             }
             Ok(())
         })
-        // .menu(menu)
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![])
         .run(ctx)
         .expect("error while running tauri application");
 }
